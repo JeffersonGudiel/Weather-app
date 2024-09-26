d874415e897e08052b12ab0869a4e0d75238780a
JeffersonGudiel <jeffersongudiel@icloud.com>
2024-09-25 21:45:40 -0600

    Improved error handling for deploy

--- app/api/fiveday/route.ts
+++ app/api/fiveday/route.ts
@@ -1,3 +1,4 @@
+"use client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -1,3 +1,4 @@
+"use client";
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
