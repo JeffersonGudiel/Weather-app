de4e17f52a41eb690ac71acdd2f12cee5e1c1767
JeffersonGudiel <jeffersongudiel@icloud.com>
2024-09-26 15:58:36 -0600

    loading message

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,15 +1,25 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  loadingMessage?: string; // Mensaje de carga opcional
+}
 
 function Skeleton({
   className,
+  loadingMessage = "🚧 Working hard to bring the best... 🚧", // Valor predeterminado para el mensaje
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: SkeletonProps) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
-      {...props}
-    />
-  )
+      className={cn(
+        "animate-pulse rounded-md bg-muted flex flex-col items-center justify-center",
+        className
+      )}
+      {...props}>
+      <span className="text-gray-500">{loadingMessage}</span>
+      {/* Puedes añadir más contenido aquí si lo deseas */}
+    </div>
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
