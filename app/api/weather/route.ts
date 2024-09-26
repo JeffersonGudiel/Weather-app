e835e26dcc7228f580ede1ec2b10d6e23cab23f2
JeffersonGudiel <jeffersongudiel@icloud.com>
2024-09-25 19:11:40 -0600

    solution to deploy

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -10,13 +10,25 @@ export async function GET(req: NextRequest) {
     const lat = searchParams.get("lat");
     const lon = searchParams.get("lon");
 
+    // Validar que los parámetros lat y lon estén presentes
+    if (!lat || !lon) {
+      return new Response("Missing latitude or longitude", { status: 400 });
+    }
+
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     const res = await axios.get(url);
 
+    // Verificar si la respuesta es correcta
+    if (res.status !== 200) {
+      return new Response("Error fetching weather data", {
+        status: res.status,
+      });
+    }
+
     return NextResponse.json(res.data);
   } catch (error) {
-    console.log("Error fetching forecast data");
+    console.error("Error fetching forecast data:", error); // Imprimir error completo
     return new Response("Error fetching forecast data", { status: 500 });
   }
 }
