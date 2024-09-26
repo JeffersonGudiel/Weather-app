16597cffc180088dd92e94062a8de812d7be5e2f
JeffersonGudiel <jeffersongudiel@icloud.com>
2024-09-25 18:52:28 -0600

    Complete project for company

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,92 @@
+"use client";
 import Image from "next/image";
+import AirPollution from "./Components/AirPollution/AirPollution";
+import DailyForecast from "./Components/DailyForecast/DailyForecast";
+import FeelsLike from "./Components/FeelsLike/FeelsLike";
+import Humidity from "./Components/Humidity/Humidity";
+import Mapbox from "./Components/Mapbox/Mapbox";
+import Navbar from "./Components/Navbar";
+import Population from "./Components/Population/Population";
+import Pressure from "./Components/Pressure/Pressure";
+import Sunset from "./Components/Sunset/Sunset";
+import Temperature from "./Components/Temperature/Temperature";
+import UvIndex from "./Components/UvIndex/UvIndex";
+import Visibility from "./Components/Visibility/Visibility";
+import Wind from "./Components/Wind/Wind";
+import defaultStates from "./utils/defaultStates";
+import FiveDayForecast from "./Components/FiveDayForecast/FiveDayForecast";
+import { useGlobalContextUpdate } from "./context/globalContext";
 
 export default function Home() {
+  const { setActiveCityCoords } = useGlobalContextUpdate();
+
+  const getClickedCityCords = (lat: number, lon: number) => {
+    setActiveCityCoords([lat, lon]);
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
+      <Navbar />
+      <div className="pb-4 flex flex-col gap-4 md:flex-row">
+        <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
+          <Temperature />
+          <FiveDayForecast />
+        </div>
+        <div className="flex flex-col w-full">
+          <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
+            <AirPollution />
+            <Sunset />
+            <Wind />
+            <DailyForecast />
+            <UvIndex />
+            <Population />
+            <FeelsLike />
+            <Humidity />
+            <Visibility />
+            <Pressure />
+          </div>
+          <div className="mapbox-con mt-4 flex gap-4">
+            <Mapbox />
+            <div className="states flex flex-col gap-3 flex-1">
+              <h2 className="flex items-center gap-2 font-medium">
+                Top Large Cities
+              </h2>
+              <div className="flex flex-col gap-4">
+                {defaultStates.map((state, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="border rounded-lg cursor-pointer dark:bg-dark-grey shadow-sm dark:shadow-none"
+                      onClick={() => {
+                        getClickedCityCords(state.lat, state.lon);
+                      }}>
+                      <p className="px-6 py-4">{state.name}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer className="py-4 flex justify-center pb-8">
+        <p className="footer-text text-sm flex items-center gap-1">
+          Copyright &copy; Jefferson. Todos los derechos reservados.
+          <Image src={"/logo.svg"} alt="logo" width={20} height={20} />
+          <a
+            href="https://www.linkedin.com/in/jefferson-alexander/"
+            target="_blank"
+            className=" text-green-300 font-bold">
+           Jefferson Gudiel.
+          </a>
+        </p>
+      </footer>
     </main>
   );
 }
